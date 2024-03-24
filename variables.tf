0235674431e4e7d99d7995f383bf316b7c741b0e
kalpanap2003 <141616961+kalpanap2003@users.noreply.github.com>
2024-03-24 20:30:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "admin-sasi-key" 
+    default     =  "kalpana1.pem" 
 }
 
 variable "instance_type" { 
